e7faa59ac3ade2ff3c1d092dc7d78f1a28dfc341
YehudaHeller10 <141673758+YehudaHeller10@users.noreply.github.com>
2025-09-09 16:56:29 +0300

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,23 @@
+// Configuration and constants
+window.APP_CONFIG = {
+    firebaseConfig: {
+        apiKey: "AIzaSyDpzAnHPl8trZDQwC-G5twRWSwdweko_T8",
+        authDomain: "work-report-volcani.firebaseapp.com",
+        projectId: "work-report-volcani",
+        storageBucket: "work-report-volcani.firebasestorage.app",
+        messagingSenderId: "569559789764",
+        appId: "1:569559789764:web:d11b9c0e43ff78a66dd991",
+        measurementId: "G-M5Z4R1FB40",
+        databaseURL: "https://work-report-volcani-default-rtdb.firebaseio.com/"
+    },
+    hoursPerDay: 8,
+    // Default global researchers list (seed)
+    defaultResearchers: [
+        "יעל זלצר",
+        "גיאורגי שטנברג",
+        "ויקטור בלוך",
+        "יפתח קלפ",
+        "ספי ורניק",
+        "אלון סלע"
+    ],
+};
